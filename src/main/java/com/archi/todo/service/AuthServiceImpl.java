3c5c1bd680a614989e26d882c04db108cb55de8f
Archisman Tripathi <archismantripathi@gmail.com>
2024-04-14 10:58:54 +0530

    update token generation strategy

--- src/main/java/com/archi/todo/service/AuthServiceImpl.java
+++ src/main/java/com/archi/todo/service/AuthServiceImpl.java
@@ -4,7 +4,6 @@ import com.archi.todo.dto.auth.LoginDTO;
 import com.archi.todo.model.UserData;
 import com.archi.todo.repository.UserRepository;
 import com.google.common.hash.Hashing;
-import org.bson.json.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 public class AuthServiceImpl implements AuthService {
@@ -21,18 +21,34 @@ public class AuthServiceImpl implements AuthService {
     private UserRepository userRepository;
     private String secret = "secret";
 
-    private String generateToken(String username,String secret) {
-        String token = Hashing.sha256()
-                .hashString(username+secret, StandardCharsets.UTF_8)
+    private String randomStringGenerator(int targetStringLength) {
+        int leftLimit = 48;     // numeral '0'
+        int rightLimit = 122;   // letter 'z'
+
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
-        return token + username;
+    }
+
+    private String generateToken(String username, String secret, String randomString) {
+        String token = Hashing.sha256()
+                .hashString(username+secret+randomString, StandardCharsets.UTF_8)
+                .toString();
+        return token + randomString + username;
     }
 
     @Override
     public String validateToken(String token) {
-        String username = token.substring(64);
-        if(token.equals(generateToken(username, this.secret)))
-            return username;
+        if(token!=null && token.length() > 80) {
+            String username = token.substring(80);
+            String randomString = token.substring(64, 80);
+            if (token.equals(generateToken(username, this.secret, randomString)))
+                return username;
+        }
         return null;
     }
 
@@ -48,10 +64,12 @@ public class AuthServiceImpl implements AuthService {
                     .hashString(loginDTO.getPassword(), StandardCharsets.UTF_8)
                     .toString();
             if(password.equals(userData.get().getPassword())) {
+                String randomString = randomStringGenerator(16);
+
                 return ResponseEntity
                         .status(HttpStatus.ACCEPTED)
                         .header("ok","true")
-                        .body(new HashMap<String, String>(){{put("message", generateToken(loginDTO.getUsername(),secret));}});
+                        .body(new HashMap<String, String>(){{put("message", generateToken(loginDTO.getUsername(), secret, randomString));}});
             }
 
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body( new HashMap<String, String>(){{put("message", "Error: Password mismatch");}});
